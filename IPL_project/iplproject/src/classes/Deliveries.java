d90d238a32ba032d758e79592ce2ad9ba4651371
Rahul Aragi <rahularagi108@gmail.com>
2023-04-26 00:08:32 +0530

    commit-2

---
+++ IPL_project/iplproject/src/classes/Deliveries.java
@@ -0,0 +1,25 @@
+package classes;
+
+public class Deliveries {
+    int match_id;
+    int inning;
+    String  batting_team;
+    String bowling_team;
+    int over;
+    int ball;
+    String batsman;
+    String non_striker;
+    String bowler;
+    int is_super_over;
+    int wide_runs;
+    int bye_runs;
+    int legbye_runs;
+    int noball_runs;
+    int penalty_runs;
+    int batsman_runs;
+    int extra_runs;
+    int total_runs;
+    String player_dismissed;
+    String dismissal_kind;
+    String fielder;
+}
