dca0d8a094eccc5c5dcd1dc7dca499cedfd96179
Rahul Aragi <rahularagi108@gmail.com>
2023-04-25 23:51:09 +0530

    commit1

---
+++ IPL_project/iplproject/src/classes/Match.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class Match {
+    int id;
+    int season;
+    String city;
+    String date;
+    String team1;
+    String team2;
+    String toss_winner;
+    String toss_decision;
+    String result;
+    int dl_applied;
+    String winner;
+    int win_by_runs;
+    int win_by_wickets;
+    String player_of_match;
+    String venue;
+    String umpire1;
+    String umpire2;
+    String umpire3;
+}
